25d9dba39a3fb998c68176d909ee2f2f8212c65a
John Mettraux <jmettraux@gmail.com>
2026-09-02 08:56:12 +0900

    Don't freeze everything here we want singleton

--- lib/scorn.rb
+++ lib/scorn.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 require 'json'
 require 'time'
@@ -11,7 +10,7 @@ require 'net/http'
 
 module Scorn
 
-  VERSION = '0.4.1'
+  VERSION = '0.4.1'.freeze
 
   class << self
 
